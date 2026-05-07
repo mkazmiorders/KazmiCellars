4b320389008b37867c4931073d08fa8980217010
mkazmiorders <mkazmiorders@gmail.com>
2026-05-07 14:49:11 -0500

    Update index.js

--- functions/api/auth/index.js
+++ functions/api/auth/index.js
@@ -3,6 +3,7 @@ export async function onRequest(context) {
   const params = new URLSearchParams({
     client_id: env.GITHUB_CLIENT_ID,
     scope: "repo,user",
+    redirect_uri: "https://kazmicellars.com/api/auth/callback",
   });
   return Response.redirect(
     `https://github.com/login/oauth/authorize?${params}`,
